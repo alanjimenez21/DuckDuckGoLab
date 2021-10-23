f5495ffad6f9dd5a64c14301a9948b39d4c5bca6
Alan Jimenez-Gallardo <aijimenezgallardo@my.waketech.edu>
2021-10-23 01:51:34 -0400

    Added Presidents

--- DuckDuckGoLab.py
+++ DuckDuckGoLab.py
@@ -4,16 +4,12 @@ import pytest
 
 url_DuckDuckGo = "https://api.duckduckgo.com/?q="
 
-@pytest.mark.parametrize("usapresidents", ["Washington", "Adams", "Jefferson", "Madison",
-                                            "Monroe", "Quincy Adams", "Jackson", "Van Buren",
-                                            "Harrison", "Tyler", "Polk", "Taylor", "Filmore",
-                                            "Pierce", "Buchanan", "Lincoln", "Filmore", "Grant",
-                                            "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison",
-                                            "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson",
-                                            "Harding", "Coolidge", "Hoover", "Roosevelt", "Truman",
-                                            "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter",
-                                            "Reagan", "H. W. Bush", "Clinton", "W. Bush", "Obama",
-                                            "Trump", "Biden"])
+@pytest.mark.parametrize("usapresidents",
+                         ["Washington", "Adams", "Jefferson", "Madison","Monroe", "Quincy Adams", "Jackson", "Van Buren",
+                          "Harrison", "Tyler", "Polk", "Taylor", "Filmore", "Pierce", "Buchanan", "Lincoln", "Filmore", "Grant",
+                          "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison", "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson",
+                          "Harding", "Coolidge", "Hoover", "Roosevelt", "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter",
+                          "Reagan", "H. W. Bush", "Clinton", "W. Bush", "Obama", "Trump", "Biden"])
 
 def test_presidents_of_the_USA(usapresidents):
     resp = requests.get(url_DuckDuckGo + "/?q=presidents+of+the+united+states&format=json")
