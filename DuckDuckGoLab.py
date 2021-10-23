db514b4940151f73bd03e9a69b7b2c93c49cc2df
Alan Jimenez-Gallardo <aijimenezgallardo@my.waketech.edu>
2021-10-23 01:29:47 -0400

    Change to Code

--- DuckDuckGoLab.py
+++ DuckDuckGoLab.py
@@ -2,10 +2,11 @@ import requests
 import pytest
 
 
-url_ddg = "https://api.duckduckgo.com"
+url_ddg = "https://api.duckduckgo.com/?q="
 
 
-def test_ddg0():
+
+def test_presidents_of_the_USA():
     resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
     rsp_data = resp.json()
     assert "DuckDuckGo" in rsp_data["Heading"]
