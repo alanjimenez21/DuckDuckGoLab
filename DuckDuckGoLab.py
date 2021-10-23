1e6e129ca675e028b33b90e11bd4edb351d77227
Alan Jimenez-Gallardo <aijimenezgallardo@my.waketech.edu>
2021-10-23 01:33:56 -0400

    Change to Code

--- DuckDuckGoLab.py
+++ DuckDuckGoLab.py
@@ -4,7 +4,8 @@ import pytest
 
 url_DuckDuckGo = "https://api.duckduckgo.com/?q="
 
+@pytest.mark.parametrize()
 
-def test_presidents_of_the_USA():
+def test_presidents_of_the_USA(usa_president):
     resp = requests.get(url_DuckDuckGo + "/?q=presidents+of+the+united+states&format=json")
     rsp_data = resp.json()
