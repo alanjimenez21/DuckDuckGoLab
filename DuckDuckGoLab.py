afb0f1d70ad050341e10d283dc2401e3c0a5f186
Alan Jimenez-Gallardo <aijimenezgallardo@my.waketech.edu>
2021-10-23 02:15:46 -0400

    Removed Quincy & Bush Senior

--- DuckDuckGoLab.py
+++ DuckDuckGoLab.py
@@ -17,17 +17,17 @@ import pytest
 url_DuckDuckGo = "https://api.duckduckgo.com/?q="
 
 #Iterate
-@pytest.mark.parametrize("usapresidents",
-                         ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Quincy Adams", "Jackson", "Van Buren",
+@pytest.mark.parametrize("usa_presidents",
+                         ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Jackson", "Van Buren",
                           "Harrison", "Tyler", "Polk", "Taylor", "Filmore", "Pierce", "Buchanan", "Lincoln", "Filmore", "Grant",
-                          "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison", "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson",
+                          "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison", "McKinley", "Roosevelt", "Taft", "Wilson",
                           "Harding", "Coolidge", "Hoover", "Roosevelt", "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter",
-                          "Reagan", "H. W. Bush", "Clinton", "W. Bush", "Obama", "Trump", "Biden"])
+                          "Reagan", "Bush", "Clinton", "Obama", "Trump", "Biden"])
 
 #Define the Test Function
-def test_presidents_of_the_USA(usapresidents):
+def test_presidents_of_the_USA(usa_presidents):
     resp = requests.get(url_DuckDuckGo + "/?q=presidents+of+the+united+states&format=json")
     rsp_data = resp.json()
     related_topics = rsp_data['RelatedTopics']
     for i in range(len(related_topics)):
-        assert usapresidents in related_topics
+        assert usa_presidents in related_topics
